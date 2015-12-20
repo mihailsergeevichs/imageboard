021582bd1698b5b5892e5cdc8cb5b6603ff64626
Mihail Sergeevich <mihailsergeevichs@gmail.com>
2015-12-20 21:53:36 +0200

    added a base superclass for entities, added DateTime libraries

--- src/test/java/com/mihailsergeevichs/imageboard/entity/BaseEntity.java
+++ src/test/java/com/mihailsergeevichs/imageboard/entity/BaseEntity.java
@@ -1,7 +1,41 @@
 package com.mihailsergeevichs.imageboard.entity;
 
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by Overlord on 20.12.2015.
  */
-public class BaseEntity {
+@MappedSuperclass
+public abstract class BaseEntity<ID> {
+
+    @Column(name = "created", nullable = false)
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime created;
+
+    @Column(name = "modified", nullable = false)
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime modified;
+
+    @Version
+    private long version;
+
+    public abstract ID getId();
+
+    @PrePersist
+    public void prePersist() {
+        DateTime current = DateTime.now();
+        this.created = current;
+        this.modified = current;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modified = DateTime.now();
+    }
+
+
 }
