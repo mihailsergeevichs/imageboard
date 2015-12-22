c5edb86bb2ec462e08316cf237749d900151e4c6
Mihail Sergeevich <mihailsergeevichs@gmail.com>
2015-12-22 18:42:24 +0200

    Changed pom configuration, some dependencies replaced for actual

--- src/main/java/com/mihailsergeevichs/imageboard/ImageboardApplication.java
+++ src/main/java/com/mihailsergeevichs/imageboard/ImageboardApplication.java
@@ -2,7 +2,9 @@ package com.mihailsergeevichs.imageboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class ImageboardApplication {
 
